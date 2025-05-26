671581ab3a9af8b3bf7be71194b4b2f0e089cf33
Gading Jaluaji <gjaluaji@gmail.com>
2025-05-26 18:35:49 +0700

    implementasi middleware tugas laravel pertemuan 6

--- routes/api.php
+++ routes/api.php
@@ -5,15 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\AuthorController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::apiResource('authors', AuthorController::class)->only(['index', 'show']);
+Route::apiResource('genres', GenreController::class)->only(['index', 'show']);
 
-Route::get('/genres', [GenreController::class, 'index']);
-Route::post('/genres', [GenreController::class, 'store']);
-
-Route::get('/authors', [AuthorController::class, 'index']);
-Route::post('/authors', [AuthorController::class, 'store']);
-
-Route::apiResource('genres', GenreController::class);
-Route::apiResource('authors', AuthorController::class);
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::apiResource('authors', AuthorController::class)->only(['store', 'update', 'destroy']);
+    Route::apiResource('genres', GenreController::class)->only(['store', 'update', 'destroy']);
